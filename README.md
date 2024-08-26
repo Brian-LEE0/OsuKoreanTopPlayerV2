d0ac4bcb77331e7104140a26375c3a307fe751d8
brian <brian@dooub.com>
2024-08-27 08:23:07 +0900

    init all java spring

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # OsuKoreanTopPlayerV2
+
+This is project for OKTP voting platform.
+
+Legacy was constructed by nodejs express and next generation is by java spring
---
+++ src/main/java/com/oktp/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.oktp.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
